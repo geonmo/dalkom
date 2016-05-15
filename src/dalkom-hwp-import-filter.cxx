fe5b0b92d859ec2a93b04a3c9b949e0e476b6567
Hodong Kim <cogniti@gmail.com>
2016-05-15 22:38:47 +0900

    Added comments
    본 제품은 한글과컴퓨터의 한글 문서 파일(.hwp) 공개 문서를 참고하여
    개발하였습니다.

--- src/dalkom-hwp-import-filter.cxx
+++ src/dalkom-hwp-import-filter.cxx
@@ -20,6 +20,11 @@
  * along with this program;  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * 본 제품은 한글과컴퓨터의 한글 문서 파일(.hwp) 공개 문서를 참고하여
+ * 개발하였습니다.
+ */
+
 #include <cppuhelper/supportsservice.hxx>
 
 #include "dalkom-hwp-import-filter.hxx"
